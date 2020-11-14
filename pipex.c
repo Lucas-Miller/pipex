e1be10bc606fd9d975210d7743f66e85040aeffd
Your Name <millerlucas25@gmail.com>
2020-11-13 23:08:34 -0500

    Fixed bugs

--- pipex.c
+++ pipex.c
@@ -31,17 +31,37 @@ int main(int ac , char **av)
     if( (pidOne = fork()) == -1)
         errorHandler("Cannot fork", 2);
     
+
+    if (pidOne > 0) {   /* Parent executes av[3] */
+        close(pipex[1]);/* Parent doesnt write to pipe */
+        if(dup2(pipex[0], 0) == -1)
+            errorHandler("Could not redirect std in", 3);
+        
+        close(pipex[0]); /* stdin is duplicated so we close pipe */
+        execlp(av[3], av[3], NULL);
+        errorHandler(av[3], 4);
+    }
+
     if( (pidTwo = fork()) == -1)
         errorHandler("Cannot fork", 2);
-
-
-    if (pidOne > 0 && pidTwo > 0) {
-        close(pipex[1]);
+    
+        if (pidOne > 0) {   /* Parent executes av[2] */
+        close(pipex[1]);/* Parent doesnt write to pipe */
         if(dup2(pipex[0], 0) == -1)
-            errorHandler("Could not redirect std output", 4);
+            errorHandler("Could not redirect std in", 3);
         
-        close(pipex[1]);
-        execlp(av[1], av[1], NULL);
-        errorHandler(av[1], 5);
+        close(pipex[0]); /* stdin is duplicated so we close pipe */
+        execlp(av[2], av[2], NULL);
+        errorHandler(av[2], 4);
     }
+    close(pipex[0]);
+
+    if(dup2(pipex[1], 1) == -1)
+        errorHandler("Could not redirect std in", 4);
+        
+    close(pipex[0]); /* stdin is duplicated so we close pipe */
+    execlp(av[1], av[1], NULL);
+    errorHandler(av[1], 5);
+    
+
 }
